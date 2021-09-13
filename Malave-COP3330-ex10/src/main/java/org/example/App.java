defdaed59f6ce0614748e2af670a787c73c0f74a
jdcoder75 <89999560+jdcoder75@users.noreply.github.com>
2021-09-13 17:54:31 -0400

    Update App.java

--- Malave-COP3330-ex10/src/main/java/org/example/App.java
+++ Malave-COP3330-ex10/src/main/java/org/example/App.java
@@ -2,8 +2,8 @@ package org.example;
 import java.util.Scanner;
 import java.util.Date;
 /*
- *  UCF COP3330 Fall 2021 Assignment 1 Solution
- *  Copyright 2021 first_name last_name
+ *  UCF COP3330 Fall 2021 Assignment 10 Solution
+ *  Copyright 2021 Jose Malave
  */
 
 public class App
